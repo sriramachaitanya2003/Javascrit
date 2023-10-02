327cd5f386707d7bfcf249530a671ace6a65bf29
Komaravolu Srirama Chaitanya Murthy <92015496+sriramachaitanya2003@users.noreply.github.com>
2023-10-02 05:55:28 +0000

    commit config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,18 +2,23 @@ let score = "333"  //It is a string
 let score1 = null
 let score2 = "33abc"
 
-console.log(typeof score)
-console.log(typeof score1)
-console.log(typeof score2)
+// console.log(typeof score) //string
+// console.log(typeof score1) // object
+// console.log(typeof score2) //string
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) 
 let valueInNumber2 = Number(score1)
 let valueInNumber3 = Number(score2)
 
-console.log(typeof valueInNumber)
-console.log(typeof valueInNumber2)
-console.log(typeof valueInNumber3)
+// console.log(typeof valueInNumber) //number
+// console.log(typeof valueInNumber2)
+// console.log(typeof valueInNumber3)
 
-console.log(valueInNumber)
-console.log(valueInNumber2)
-console.log(valueInNumber3)
+// console.log(valueInNumber)  //333
+// console.log(valueInNumber2)  //0
+// console.log(valueInNumber3)  //NaN(Not a Number)
+
+let number = 3
+let negNumber = -number
+// console.log(negNumber);
+
