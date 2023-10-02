633d2879b455cae19a5b9bae0c6dd9679b43b74d
Komaravolu Srirama Chaitanya Murthy <92015496+sriramachaitanya2003@users.noreply.github.com>
2023-10-02 06:10:25 +0000

    comparision of datatypes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,6 +5,9 @@ console.log(null > 0)
 console.log(null == 0)
 console.log(null >= 0)
 
-console.log(undefined = 0)
+// In comparision of null and numbers, when equality operator is used, null is considered as null
+// when other comparision operators are used null is considered as zero 
+
+console.log(undefined == 0)
 console.log(undefined > 0)
 console.log(undefined < 0)
