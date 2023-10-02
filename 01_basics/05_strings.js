9a0d2a997a1aa76086c425078c0b43ed9cda331f
Komaravolu Srirama Chaitanya Murthy <92015496+sriramachaitanya2003@users.noreply.github.com>
2023-10-02 07:32:33 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,28 @@
 const myName = "sriram"
 let myAge = 20
 
-console.log(myName + myAge);  //outdated
+// console.log(myName + myAge);  //outdated
 
-console.log(`My name is ${myName} and I am ${myAge} years old`);
+// console.log(`My name is ${myName} and I am ${myAge} years old`);
 
+const fullName = new String('Srirama Chaitanya')
+
+// console.log(fullName[2])
+// console.log(fullName.__proto__)
+// console.log(fullName.length)
+// console.log(fullName.toUpperCase())
+// console.log(fullName.charAt(5))
+// console.log(fullName.indexOf('a'))
+
+const gameName = fullName.substring(0,7)
+console.log(gameName);
+
+const newString = gameName.slice(-4,7)
+console.log(newString)
+
+const anotherString = "   Chaitanya   "
+console.log(anotherString)
+console.log(anotherString.trim())
+
+const newerstring = "Hello World"
+console.log(newerstring.replace('World','sriram'))
