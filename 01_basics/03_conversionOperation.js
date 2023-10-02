1a0266cdabc99884505ecf20d030c2088494311a
Komaravolu Srirama Chaitanya Murthy <92015496+sriramachaitanya2003@users.noreply.github.com>
2023-10-02 05:29:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "333"  //It is a string
+let score1 = null
+let score2 = "33abc"
+
+console.log(typeof score)
+console.log(typeof score1)
+console.log(typeof score2)
+
+let valueInNumber = Number(score)
+let valueInNumber2 = Number(score1)
+let valueInNumber3 = Number(score2)
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber2)
+console.log(typeof valueInNumber3)
+
+console.log(valueInNumber)
+console.log(valueInNumber2)
+console.log(valueInNumber3)
