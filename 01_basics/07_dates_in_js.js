c245842d6a4929fa1dbf11f2964ce45e4f1efde6
Komaravolu Srirama Chaitanya Murthy <92015496+sriramachaitanya2003@users.noreply.github.com>
2023-10-09 04:48:00 +0000

    Dates in JS

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,12 @@
+let date = new Date()
+
+console.log(date);
+console.log(date.toString())
+console.log(date.toDateString())
+console.log(date.toISOString())
+console.log(date.toJSON())
+console.log(date.toLocaleDateString())
+console.log(date.toLocaleString())
+
+const date2 = new Date(2003, 3 ,11)
+console.log(date2.toDateString());
